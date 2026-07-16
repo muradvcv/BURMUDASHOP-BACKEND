ec5846baf89b7a1108fbb5497f3f6f4e88f8c147
muradvcv <muradvcv@gmail.com>
2026-07-16 07:22:14 +0600

    create add product api

--- index.ts
+++ index.ts
@@ -1,7 +1,52 @@
-import express from 'express';
-const app = express();
-const port=5000;
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import { MongoClient, ServerApiVersion } from "mongodb";
 
-app.listen (port,()=>{
-  console.log(`server run on port ${port}`);
-})
+dotenv.config();
+
+const app = express();
+const port = 5000;
+
+app.use(express.json());
+app.use(cors());
+
+const client = new MongoClient(process.env.MONGODB_URI as string, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function startServer() {
+  try {
+    await client.connect();
+    console.log("successfully connected to MongoDB");
+
+    const db = client.db("burmuda_shop");
+    const productsCollection = db.collection("products");
+
+
+    //  post api for adding products to the database
+    app.post("/api/products", async (req, res) => {
+      const product = req.body;
+      const result =await productsCollection.insertOne(product);
+      res.send(result);
+    })
+
+
+
+    app.get("/", (req, res) => {
+      res.send(" Server is running successfully");
+    });
+
+    app.listen(port, () => {
+      console.log(` Server running on port ${port}`);
+    });
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+startServer();
